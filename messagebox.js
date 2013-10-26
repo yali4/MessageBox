0ad97bbf8038485d6816526e4c882e0133a92540
Yalçın Ceylan <yalcinceylan93@gmail.com>
2013-10-26 10:03:18 +0300

    Kullanım ve diğer ayarlar değiştirildi.
    Kullanımı değiştirildi.
    Çoklu bildirim kutularının çakışmadan çalışması sağlandı.

--- messagebox.js
+++ messagebox.js
@@ -11,46 +11,48 @@
 
 ;(function($) {
 
-	$.fn.MessageBoxactive = false;
+	$.fn.MessageBox = function(event,options,callback,preventDefault,stopPropagation) {
 
-	$.fn.MessageBox = function(options) {
-	
-		$(this.selector).each(function() {
-			$.MessageBox.call($(this), options);
+		var target = $(this);
+		target[event](function(event) {
+			if (preventDefault && preventDefault == true) {
+					event.returnValue = false;
+					if (event.preventDefault) { event.preventDefault(); }
+			}
+			if (stopPropagation && stopPropagation == true) {
+					event.cancelBubble = true;
+					if (event.stopPropagation) { event.stopPropagation(); }
+			}
+			if (target.blur) { target.blur(); }
+			$.MessageBox(options,callback,target);
 		});
-		
+
 	};
 
-	$.MessageBox = function(options) {
+	$.MessageBoxArray = new Array();
+
+	$.MessageBox = function(options,callback,target) {
 
 		var options = $.extend({
-			title: 'Title',
-			content: 'Description',
-			type: 'information', transaction: 'click',
-			animate : { open: false, close : false },
+			title: 'Title', content: 'Description', type: 'information',
 			buttons: { confirm: {title:'Continue', style:'continue'}, cancel: {title:'Cancel', style:'cancel'} },
 			style: { modal: 'messagebox-background', dialog: 'messagebox-dialog', content : 'messagebox-content', buttons: 'messagebox-buttons', timeout: 'messagebox-timer' },
-			background: '#000', opacity: '0.8',
+			background: '#000', opacity: '0.6',
+			animate : { open: false, close : false, speed: false },
 			timeout: { second: false, screen: false },
-			modalclose: false, usekey: false, delay: false,
-			preventDefault: false, stopPropagation: false,
-			callback: function() {}
+			modalclose: false, usekey: false
 		}, options);
 
 		var modalbox = {};
-		var clickbox = this;
 		
 		modalbox.createDialog = function() {
 
-			modalbox.modalBackground = $('<div/>', {'class':options.style.modal,css:{background:options.background,opacity:options.opacity}});
-			modalbox.modalBackground.appendTo('body');
-
-			if (options.delay) {
-				modalbox.modalBackground.css({'display':'none'}).fadeIn(options.delay);
-			}
+			modalbox.background = $('<div/>', {'class':options.style.modal,css:{background:options.background,opacity:options.opacity}});
+			modalbox.background.css({'display':'none'});
+			modalbox.background.appendTo('body');
 
 			if (options.modalclose) {
-				modalbox.modalBackground.click(function() {
+				modalbox.background.click(function() {
 					modalbox.response = false;
 					modalbox.removeDialog();
 				});
@@ -89,7 +91,7 @@
 					});
 				}
 			}
-
+			
 			modalbox.dialog.appendTo('body');
 			modalbox.width = modalbox.dialog.width();
 			modalbox.height = modalbox.dialog.height();
@@ -100,40 +102,51 @@
 			modalbox.bottom = (modalbox.windowHeight-modalbox.height);
 			modalbox.right = (modalbox.windowWidth-modalbox.width);
 
-			if (options.delay && options.animate.open) {
+			if (options.animate.speed && options.animate.open) {
 				switch(options.animate.open) {
 					case 'top':
-						modalbox.dialog.css({opacity:0,left:modalbox.leftCenter}).animate({opacity:1,top:modalbox.topCenter},options.delay);
+						modalbox.background.fadeIn(options.animate.speed);
+						modalbox.dialog.css({opacity:0,left:modalbox.leftCenter}).animate({opacity:1,top:modalbox.topCenter},options.animate.speed);
 					break;
 					case 'left':
-						modalbox.dialog.css({opacity:0,top:modalbox.topCenter}).animate({opacity:1,left:modalbox.leftCenter},options.delay);
+						modalbox.background.fadeIn(options.animate.speed);
+						modalbox.dialog.css({opacity:0,top:modalbox.topCenter}).animate({opacity:1,left:modalbox.leftCenter},options.animate.speed);
 					break;
 					case 'right':
-						modalbox.dialog.css({opacity:0,top:modalbox.topCenter,left:modalbox.right}).animate({opacity:1,left:modalbox.leftCenter},options.delay);
+						modalbox.background.fadeIn(options.animate.speed);
+						modalbox.dialog.css({opacity:0,top:modalbox.topCenter,left:modalbox.right}).animate({opacity:1,left:modalbox.leftCenter},options.animate.speed);
 					break;
 					case 'bottom':
-						modalbox.dialog.css({opacity:0,left:modalbox.leftCenter,top:modalbox.bottom}).animate({opacity:1,top:modalbox.topCenter},options.delay);
+						modalbox.background.fadeIn(options.animate.speed);
+						modalbox.dialog.css({opacity:0,left:modalbox.leftCenter,top:modalbox.bottom}).animate({opacity:1,top:modalbox.topCenter},options.animate.speed);
 					break;
 					case 'topLeft':
-						modalbox.dialog.css({opacity:0,top:0,left:0}).animate({opacity:1,top:modalbox.topCenter,left:modalbox.leftCenter},options.delay);
+						modalbox.background.fadeIn(options.animate.speed);
+						modalbox.dialog.css({opacity:0,top:0,left:0}).animate({opacity:1,top:modalbox.topCenter,left:modalbox.leftCenter},options.animate.speed);
 					break;
 					case 'topRight':
-						modalbox.dialog.css({opacity:0,top:0,left:modalbox.right}).animate({opacity:1,top:modalbox.topCenter,left:modalbox.leftCenter},options.delay);
+						modalbox.background.fadeIn(options.animate.speed);
+						modalbox.dialog.css({opacity:0,top:0,left:modalbox.right}).animate({opacity:1,top:modalbox.topCenter,left:modalbox.leftCenter},options.animate.speed);
 					break;
 					case 'bottomLeft':
-						modalbox.dialog.css({opacity:0,top:modalbox.bottom,left:0}).animate({opacity:1,top:modalbox.topCenter,left:modalbox.leftCenter},options.delay);
+						modalbox.background.fadeIn(options.animate.speed);
+						modalbox.dialog.css({opacity:0,top:modalbox.bottom,left:0}).animate({opacity:1,top:modalbox.topCenter,left:modalbox.leftCenter},options.animate.speed);
 					break;
 					case 'bottomRight':
-						modalbox.dialog.css({opacity:0,top:modalbox.bottom,left:modalbox.right}).animate({opacity:1,top:modalbox.topCenter,left:modalbox.leftCenter},options.delay);
+						modalbox.background.fadeIn(options.animate.speed);
+						modalbox.dialog.css({opacity:0,top:modalbox.bottom,left:modalbox.right}).animate({opacity:1,top:modalbox.topCenter,left:modalbox.leftCenter},options.animate.speed);
 					break;
 					case 'topFade':
-						modalbox.dialog.css({opacity:0,top:(modalbox.topCenter-25),left:modalbox.leftCenter}).animate({opacity:1,top:modalbox.topCenter},options.delay);
+						modalbox.background.fadeIn(options.animate.speed);
+						modalbox.dialog.css({opacity:0,top:(modalbox.topCenter-25),left:modalbox.leftCenter}).animate({opacity:1,top:modalbox.topCenter},options.animate.speed);
 					break;
 					case 'bottomFade':
-						modalbox.dialog.css({opacity:0,top:(modalbox.topCenter+25),left:modalbox.leftCenter}).animate({opacity:1,top:modalbox.topCenter},options.delay);
+						modalbox.background.fadeIn(options.animate.speed);
+						modalbox.dialog.css({opacity:0,top:(modalbox.topCenter+25),left:modalbox.leftCenter}).animate({opacity:1,top:modalbox.topCenter},options.animate.speed);
 					break;
 				}
-			}	else {
+			} else {
+				modalbox.background.show();
 				modalbox.dialog.css({left:modalbox.leftCenter,top:modalbox.topCenter});
 			}
 			
@@ -187,181 +200,197 @@
 				target.detachEvent(event,callback,useCapture);
 			}
 		}
+		
+		modalbox.getUniqueId = function(randLength) {
+			var numbers = [];
+			for (var i=0; i<randLength; i++) {
+				numbers.push(Math.floor(Math.random()*10));
+			}
+			return numbers.join('');
+		}
 
-		modalbox.closeDialog = function() {
-			modalbox.dialog.remove();
-			modalbox.modalBackground.remove();
-			$.fn.MessageBoxactive = false;
-			modalbox.removeEventListener(window,'resize',modalPosition,false);
-			modalbox.removeEventListener(window,'onresize',modalPosition,false);
+		modalbox.addUseKey = function() {
+			if (options.usekey) {
+				modalbox.addEventListener(document,'keydown',modalKeydown,false);
+				modalbox.addEventListener(document,'onkeydown',modalKeydown,false);
+			}
+		}
+
+		modalbox.removeUseKey = function() {
 			if (options.usekey) {
 				modalbox.removeEventListener(document,'keydown',modalKeydown,false);
 				modalbox.removeEventListener(document,'onkeydown',modalKeydown,false);
 			}
+		}
+		
+		modalbox.closeDialog = function() {
+			modalbox.dialog.remove();
+			modalbox.background.remove();
+			modalbox.removeEventListener(window,'resize',modalPosition,false);
+			modalbox.removeEventListener(window,'onresize',modalPosition,false);
+			modalbox.removeUseKey();
 			if (typeof parseInt(options.timeout.second) == 'number' && parseInt(options.timeout.second) > 0) {
 				clearInterval(modalbox.interval);
 			}
-			if ( options.callback && typeof options.callback === 'function' ) {
-				options.callback.call(clickbox,modalbox.response);
+			if (typeof callback === 'function') {
+				callback.call(target,modalbox.response);
+			}
+			delete $.MessageBoxArray[modalbox.id];
+			var LastMessageBox = false;
+			for ( var i in $.MessageBoxArray ) {
+				LastMessageBox = i;
+			}
+			if (LastMessageBox) {
+				$.MessageBoxArray[LastMessageBox].show();
 			}
 		}
 
 		modalbox.removeDialog = function() {
-			if ($.fn.MessageBoxactive==true) {
-				if (typeof modalbox.response === 'undefined') {
-					modalbox.response = false;
-				}
-				if (options.delay && options.animate.close) {
-					switch(options.animate.close) {
-						case 'top':
-							modalbox.dialog.animate({top:"+=50"},'fast',function() {
-								modalbox.modalBackground.fadeOut(options.delay);
-								modalbox.dialog.animate({opacity:0,top:0},options.delay, function(){
-									modalbox.closeDialog();
-								});
-							});
-						break;
-						case 'left':
-							modalbox.dialog.animate({left:"+=50"},'fast',function() {
-								modalbox.modalBackground.fadeOut(options.delay);
-								modalbox.dialog.animate({opacity:0,left:0},options.delay, function(){
-									modalbox.closeDialog();
-								});
-							});
-						break;
-						case 'right':
-							modalbox.dialog.animate({left:"-=50"},'fast',function() {
-								modalbox.modalBackground.fadeOut(options.delay);
-								modalbox.dialog.animate({opacity:0,left:modalbox.right},options.delay, function(){
-									modalbox.closeDialog();
-								});
-							});
-						break;
-						case 'bottom':
-							modalbox.dialog.animate({top:"-=50"},'fast',function() {
-								modalbox.modalBackground.fadeOut(options.delay);
-								modalbox.dialog.animate({opacity:0,top:modalbox.bottom},options.delay, function(){
-									modalbox.closeDialog();
-								});
-							});
-						break;
-						case 'topLeft':
-							modalbox.dialog.animate({left:"+=50",top:"+=50"},'fast', function(){
-								modalbox.modalBackground.fadeOut(options.delay);
-								modalbox.dialog.animate({opacity:0,left:0,top:0},options.delay, function(){
-									modalbox.closeDialog();
-								});
-							});
-						break;
-						case 'topRight':
-							modalbox.dialog.animate({left:"-=50",top:"+=50"},'fast', function(){
-								modalbox.modalBackground.fadeOut(options.delay);
-								modalbox.dialog.animate({opacity:0,left:modalbox.right,top:0},options.delay, function(){
-									modalbox.closeDialog();
-								});
-							});
-						break;
-						case 'bottomLeft':
-							modalbox.dialog.animate({left:"+=50",top:"-=50"},'fast', function(){
-								modalbox.modalBackground.fadeOut(options.delay);
-								modalbox.dialog.animate({opacity:0,left:0,top:modalbox.bottom},options.delay, function(){
-									modalbox.closeDialog();
-								});	
-							});
-						break;
-						case 'bottomRight':
-							modalbox.dialog.animate({left:"-=50",top:"-=50"},'fast', function(){
-								modalbox.modalBackground.fadeOut(options.delay);
-								modalbox.dialog.animate({opacity:0,left:modalbox.right,top:modalbox.bottom},options.delay, function(){
-									modalbox.closeDialog();
-								});
-							});
-						break;
-						case 'fadeOut':
-							modalbox.modalBackground.fadeOut(options.delay);
-							modalbox.dialog.animate({opacity:0},options.delay, function(){
+		
+			if (typeof modalbox.response === 'undefined') {
+				modalbox.response = false;
+			}
+			if (options.animate.speed && options.animate.close) {
+				switch(options.animate.close) {
+					case 'top':
+						modalbox.dialog.animate({top:"+=50"},'fast',function() {
+							modalbox.background.fadeOut(options.animate.speed);
+							modalbox.dialog.animate({opacity:0,top:0},options.animate.speed, function(){
 								modalbox.closeDialog();
 							});
-						break;
-					}
-				} else {
-					modalbox.closeDialog();
+						});
+					break;
+					case 'left':
+						modalbox.dialog.animate({left:"+=50"},'fast',function() {
+							modalbox.background.fadeOut(options.animate.speed);
+							modalbox.dialog.animate({opacity:0,left:0},options.animate.speed, function(){
+								modalbox.closeDialog();
+							});
+						});
+					break;
+					case 'right':
+						modalbox.dialog.animate({left:"-=50"},'fast',function() {
+							modalbox.background.fadeOut(options.animate.speed);
+							modalbox.dialog.animate({opacity:0,left:modalbox.right},options.animate.speed, function(){
+								modalbox.closeDialog();
+							});
+						});
+					break;
+					case 'bottom':
+						modalbox.dialog.animate({top:"-=50"},'fast',function() {
+							modalbox.background.fadeOut(options.animate.speed);
+							modalbox.dialog.animate({opacity:0,top:modalbox.bottom},options.animate.speed, function(){
+								modalbox.closeDialog();
+							});
+						});
+					break;
+					case 'topLeft':
+						modalbox.dialog.animate({left:"+=50",top:"+=50"},'fast', function(){
+							modalbox.background.fadeOut(options.animate.speed);
+							modalbox.dialog.animate({opacity:0,left:0,top:0},options.animate.speed, function(){
+								modalbox.closeDialog();
+							});
+						});
+					break;
+					case 'topRight':
+						modalbox.dialog.animate({left:"-=50",top:"+=50"},'fast', function(){
+							modalbox.background.fadeOut(options.animate.speed);
+							modalbox.dialog.animate({opacity:0,left:modalbox.right,top:0},options.animate.speed, function(){
+								modalbox.closeDialog();
+							});
+						});
+					break;
+					case 'bottomLeft':
+						modalbox.dialog.animate({left:"+=50",top:"-=50"},'fast', function(){
+							modalbox.background.fadeOut(options.animate.speed);
+							modalbox.dialog.animate({opacity:0,left:0,top:modalbox.bottom},options.animate.speed, function(){
+								modalbox.closeDialog();
+							});	
+						});
+					break;
+					case 'bottomRight':
+						modalbox.dialog.animate({left:"-=50",top:"-=50"},'fast', function(){
+							modalbox.background.fadeOut(options.animate.speed);
+							modalbox.dialog.animate({opacity:0,left:modalbox.right,top:modalbox.bottom},options.animate.speed, function(){
+								modalbox.closeDialog();
+							});
+						});
+					break;
+					case 'fadeOut':
+						modalbox.background.fadeOut(options.animate.speed);
+						modalbox.dialog.animate({opacity:0},options.animate.speed, function(){
+							modalbox.closeDialog();
+						});
+					break;
 				}
+			} else {
+				modalbox.closeDialog();
 			}
-		}
-		
-		modalbox.insertDialog = function() {
-			if ($.fn.MessageBoxactive==false) {
-				$.fn.MessageBoxactive = true;
-				modalbox.createDialog();
-				modalbox.addEventListener(window,'resize',modalPosition,false);
-				modalbox.addEventListener(window,'onresize',modalPosition,false);
-				if (options.usekey) {
-					modalbox.addEventListener(document,'keydown',modalKeydown,false);
-					modalbox.addEventListener(document,'onkeydown',modalKeydown,false);
-				}
-				if ( typeof parseInt(options.timeout.second) == 'number' && parseInt(options.timeout.second) > 0 ) {
-					modalbox.remaining = parseInt(options.timeout.second);
-					modalbox.timeout.html(modalbox.remaining);
-					if (options.timeout.screen) {
-						modalbox.timeout.css({'display':'block'});
-					}
-					modalbox.interval = setInterval(function() {
-						if (modalbox.remaining>0) {
-							modalbox.remaining--;
-							modalbox.timeout.html(modalbox.remaining);
-						}
-						if (modalbox.remaining==0) {
-							modalbox.removeDialog();
-						}
-					},1000);
-					
-				}
-				
-				return {
-					changeTitle: function(title) {
-						modalbox.title.html(title);
-					},
-					changeContent: function(content) {
-						modalbox.description.html(content);
-					},
-					changeCallback: function(callback) {
-						if ( typeof callback === 'function' ) {
-							options.callback = callback;
-						}
-					},
-					hide: function() {
-						modalbox.dialog.hide();
-						modalbox.modalBackground.hide();				
-					},
-					show: function() {
-						modalbox.dialog.show();
-						modalbox.modalBackground.show();			
-					},
-					close: function() {
-						modalbox.removeDialog();
-					}
-				};
-				
-			}
+
 		}
 
-		if (typeof clickbox[options.transaction]==='function') {
-			clickbox[options.transaction](function(event) {
-				if (options.preventDefault) {
-					event.returnValue = false;
-					if (event.preventDefault) { event.preventDefault(); }
+		modalbox.insertDialog = function() {
+
+			for ( var i in $.MessageBoxArray ) {
+				$.MessageBoxArray[i].hide();
+			}
+
+			if (typeof modalbox.id === 'undefined') {
+				modalbox.id = modalbox.getUniqueId(48);
+			}
+
+			modalbox.createDialog();
+			modalbox.addEventListener(window,'resize',modalPosition,false);
+			modalbox.addEventListener(window,'onresize',modalPosition,false);
+			modalbox.addUseKey();
+			
+			if ( typeof parseInt(options.timeout.second) == 'number' && parseInt(options.timeout.second) > 0 ) {
+				modalbox.remaining = parseInt(options.timeout.second);
+				modalbox.timeout.html(modalbox.remaining);
+				if (options.timeout.screen) {
+					modalbox.timeout.css({'display':'block'});
 				}
-				if (options.stopPropagation) {
-					event.cancelBubble = true;
-					if (event.stopPropagation) { event.stopPropagation(); }
+				modalbox.interval = setInterval(function() {
+					if (modalbox.remaining>0) {
+						modalbox.remaining--;
+						modalbox.timeout.html(modalbox.remaining);
+					}
+					if (modalbox.remaining==0) {
+						modalbox.removeDialog();
+					}
+				},1000);
+				
+			}
+			
+			output = {
+				title: function(title) {
+					modalbox.title.html(title);
+				},
+				content: function(content) {
+					modalbox.description.html(content);
+				},
+				hide: function() {
+					modalbox.removeUseKey();
+					modalbox.dialog.hide();
+					modalbox.background.hide();
+				},
+				show: function() {
+					modalbox.addUseKey();
+					modalbox.dialog.fadeIn();
+					modalbox.background.fadeIn();	
+				},
+				close: function() {
+					modalbox.removeDialog();
 				}
-				if (typeof clickbox.blur==='function') { clickbox.blur(); }
-				return modalbox.insertDialog();
-			});
-		} else {
-			return modalbox.insertDialog();
+			};
+
+			$.MessageBoxArray[modalbox.id] = output;
+			
+			return output;
+
 		}
+
+		return modalbox.insertDialog();
 
 	}
 
